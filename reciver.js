ccdc09373f80d0386122748b2a7647d7b66c282b
wzy <yoyo_524@hotmail.com>
2011-12-29 19:27:28 +0800

    fix number format bug

--- reciver.js
+++ reciver.js
@@ -22,19 +22,22 @@ app.post('/hangqing', function(req, res) {
     var amount = req.body.amount;
     if(amount > 100000) {
         amount = amount / 10000;
+        amount.toFixed(2);
         amount += '万';
     }
     amount += '手';
     var volum = req.body.volum;
     if(volum > 100000) {
         volum = volum / 10000;
+        volum.toFixed(2);
         volum += '万';
     } else if (volum > 100000000) {
         volum = volum / 100000000;
+        volum.toFixed(2);
         volum += '亿';
     }
     volum += '元';
-    var content = '【'+req.body.name+showtype+'播报】昨收：'+req.body.close+'，今开：'+req.body.open+'，最高：'+req.body.high+'，最低：'+req.body.low+'，最新：'+req.body.price+'，涨跌额：'+req.body.updown+'，涨跌幅：'+req.body.markup+'%，总量：'+amount+'，金额：'+volum+'。';
+    var content = '【'+req.body.name+showtype+'播报】昨收：'+req.body.close+'，今开：'+req.body.open+'，最高：'+req.body.high+'，最低：'+req.body.low+'，最新：'+req.body.price+'，涨跌额：'+req.body.updown+'，涨跌幅：'+req.body.markup+'%，总量：'+volum+'，金额：'+amount+'。';
     if( myHour > 11 && req.body.type == 0) {
         var stockcode = req.body.stockcode.substr(-6);
         request({ uri:'http://172.16.39.102/interface_zjlxjiekou.php?code='+stockcode }, function (error, response, body) {
