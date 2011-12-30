4124691af689a71ea13eb01c957922d37925a149
wzy <yoyo_524@hotmail.com>
2011-12-30 11:11:08 +0800

    fix bug

--- reciver.js
+++ reciver.js
@@ -1,9 +1,9 @@
-var settings = require('./etc/settings').settings;
-var queue = require('../queuer/lib/queue');
+var settings = require('./etc/settings');
+var queue = require('queuer');
 var q = queue.getQueue(settings.queue.host, settings.queue.contentqueue);
+var request = require('request');
 var eyes = require('eyes');
 var express = require('express');
-var request = require('request');
 var app = express.createServer();
 app.use(express.bodyParser());
 var MySqlClient = require('./lib/mysql').MySqlClient;
@@ -20,25 +20,26 @@ app.post('/hangqing', function(req, res) {
     } else {
         var showtype = '午盘';
     }
-    var amount = req.body.amount;
+    var amount = req.body.volum;
     if(amount > 100000) {
         amount = amount / 10000;
-        amount.toFixed(2);
+        amount = amount.toFixed(2);
         amount += '万';
     }
     amount += '手';
-    var volum = req.body.volum;
-    if(volum > 100000) {
-        volum = volum / 10000;
-        volum.toFixed(2);
-        volum += '万';
-    } else if (volum > 100000000) {
+    var volum = req.body.amount;
+    if (volum > 100000000) {
         volum = volum / 100000000;
-        volum.toFixed(2);
+        volum = volum.toFixed(2);
         volum += '亿';
+    } else if(volum > 100000) {
+        volum = volum / 10000;
+        volum = volum.toFixed(2);
+        volum += '万';
     }
     volum += '元';
-    var content = '【'+req.body.name+showtype+'播报】昨收：'+req.body.close+'，今开：'+req.body.open+'，最高：'+req.body.high+'，最低：'+req.body.low+'，最新：'+req.body.price+'，涨跌额：'+req.body.updown+'，涨跌幅：'+req.body.markup+'%，总量：'+volum+'，金额：'+amount+'。';
+    var content = '【'+req.body.name+showtype+'播报】昨收：'+req.body.close+'，今开：'+req.body.open+'，最高：'+req.body.high+'，最低：'+req.body.low+'，最新：'+req.body.price+'，涨跌额：'+req.body.updown+'，涨跌幅：'+req.body.markup+'%，总量：'+amount+'，金额：'+volum+'。';
+    console.log(content);
     if( myHour > 11 && req.body.type == 0) {
         var stockcode = req.body.stockcode.substr(-6);
         request({ uri:settings.zjlx+stockcode }, function (error, response, body) {
