8cde355dec1247d8ad57a52fefa4e9e6a4f64ae5
wzy <yoyo_524@hotmail.com>
2012-01-04 14:49:39 +0800

    fix bug

--- reciver.js
+++ reciver.js
@@ -1,6 +1,6 @@
 var settings = require('./etc/settings');
-//var queue = require('queuer');
-//var q = queue.getQueue(settings.queue.host, settings.queue.contentqueue);
+var queue = require('queuer');
+var q = queue.getQueue(settings.queue.host, settings.queue.contentqueue);
 var request = require('request');
 var express = require('express');
 var app = express.createServer();
@@ -44,27 +44,24 @@ app.post('/hangqing', function(req, res) {
             if(error) {
                 console.log('request error:'+stockcode);
                 res.end('error! request error:'+stockcode);
-                return;
             }
             if(response.statusCode != 200) {
                 res.end('error! status error:'+stockcode);
-                return;
             }
             try {
                 var oData = JSON.parse(body);
                 content += '【资金流向】净流量:' + oData.stock.fundquantity + '万元，其中机构：'+oData.stock.jigou.jigouquantity+'万元，大户'+oData.stock.dahu.dahuquantity+'万元，散户'+oData.stock.sanhu.sanhuquantity+'万元。';
-                mysql.insert_micro_blog(req.body.stockcode, iTimestring, content, function(blogid) {
-                    if(blogid > 0) {
-                        //q.enqueue('mysql://'+settings.mysql.host+':'+settings.mysql.port+'/'+settings.mysql.database+'?micro_blog#'+blogid);
-                        res.end('success');
-                    } else {
-                        res.end('error! insert error:'+stockcode);
-                    }
-                });
             } catch(err) {
                 console.log('parse error12:'+stockcode);
                 res.end('error! parse error:'+stockcode);
-                return;
+            }
+        });
+        mysql.insert_micro_blog(req.body.stockcode, iTimestring, content, function(blogid) {
+            if(blogid > 0) {
+                q.enqueue('mysql://'+settings.mysql.host+':'+settings.mysql.port+'/'+settings.mysql.database+'?micro_blog#'+blogid);
+                res.end('success');
+            } else {
+                res.end('error! insert error:'+stockcode);
             }
         });
     } else {
