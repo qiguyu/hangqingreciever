604b64a4bccedc3b3a04dd462e653543bef38434
wzy <yoyo_524@hotmail.com>
2012-01-17 21:15:36 +0800

    增加图片功能

--- receiver.js
+++ receiver.js
@@ -1,6 +1,9 @@
 var settings = require('./etc/settings.json');
 var request = require('request');
 var weibo = require('./lib/weibo');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var app = express.createServer();
 
@@ -11,7 +14,7 @@ app.post('/hangqing', function(req, res) {
     var myMin = myDate.getMinutes();
     if(req.body.stockcode == undefined || req.body.name == undefined || req.body.date == undefined || req.body.stockcode.length != 8) {
         res.end('error! invalid stock code or name');
-    } else if(myHour > 15 || myHour < 9 || (myHour == 15 && myMin > 30) || (myHour == 9 && myMin < 20)) {
+    } else if(myHour > 25 || myHour < 9 || (myHour == 15 && myMin > 30) || (myHour == 9 && myMin < 20)) {
         res.end('not work now!');
     } else {
         var sToday = req.body.date.substr(0, 8);
@@ -85,13 +88,14 @@ app.post('/hangqing', function(req, res) {
         content += '，成交：'+volum+'（'+amount+'），换手率：'+hqSwaprate.toFixed(2)+'%';
         if( hqHour > 10 && req.body.type == 1) {
             var stockcode = req.body.stockcode.substr(-6);
-            request({ uri:settings.zjlx+stockcode }, function (error, response, body) {
+            request({ uri:settings.zjlx.api+stockcode }, function (error, response, body) {
                 if(error || response.statusCode != 200) {
                     console.log('request error:'+stockcode);
                 }
                 try {
                     var oData = JSON.parse(body);
                     if(oData.message == undefined || oData.stock.datetime.substr(0, 8) == sToday) {
+                        var myPic = '';
                         var zjQuantiti = oData.stock.fundquantity;
                         if(typeof zjQuantiti == 'string') {
                             zjQuantiti = parseFloat(zjQuantiti);
@@ -109,20 +113,46 @@ app.post('/hangqing', function(req, res) {
                             zjShQuantiti = parseFloat(zjShQuantiti);
                         }
                         content += '【资金流向】净流量：' + zjQuantiti.toFixed(2) + '万元（机构：'+zjJgQuantiti.toFixed(2)+'万元，大户：'+zjDhQuantiti.toFixed(2)+'万元，散户：'+zjShQuantiti.toFixed(2)+'万元）';
+                        var imageUri = weibo.parseUrltoObj(settings.zjlx.image + stockcode + ".png");
+                        getWeiboPicFolder(sToday, function(imageFolder) {
+                            http.get(imageUri, function(resp) {
+                                var buffers = [], size = 0;
+                                resp.on('data', function(buffer) {
+                                    buffers.push(buffer);
+                                    size += buffer.length;
+                                });
+                                resp.on('end', function() {
+                                    myPic = imageFolder+'/'+stockcode+'_zjlx_'+hqHour+'.png';
+                                    var buffer = new Buffer(size), pos = 0;
+                                    for(var i = 0, len = buffers.length; i < len; i++) {
+                                        buffers[i].copy(buffer, pos);
+                                        pos += buffers[i].length;
+                                    }
+                                    fs.writeFile(myPic, buffer);
+                                    weibo.addWeibo(req.body.stockcode, content, myPic, function(blogid) {
+                                        if(blogid > 0) {
+                                            res.end('success');
+                                        } else {
+                                            res.end('error');
+                                        }
+                                    });
+                                });
+                            });
+                        });
                     }
                 } catch(err) {
                     console.log('parse error12:'+stockcode);
+                    weibo.addWeibo(req.body.stockcode, content, '', function(blogid) {
+                        if(blogid > 0) {
+                            res.end('success');
+                        } else {
+                            res.end('error');
+                        }
+                    });
                 }
-                weibo.addWeibo(req.body.stockcode, content, function(blogid) {
-                    if(blogid > 0) {
-                        res.end('success');
-                    } else {
-                        res.end('error');
-                    }
-                });
             });
         } else {
-            weibo.addWeibo(req.body.stockcode, content, function(blogid) {
+            weibo.addWeibo(req.body.stockcode, content, '', function(blogid) {
                 if(blogid > 0) {
                     res.end('success');
                 } else {
@@ -136,13 +166,68 @@ app.post('/weibo', function(req, res) {
     if(req.body.stockcode == undefined || req.body.content == undefined || req.body.stockcode.length != 8) {
         res.end('invalied stockcode or content!');
     } else {
-        weibo.addWeibo(req.body.stockcode, req.body.content, function(blogid) {
-            if(blogid > 0) {
-                res.end('success');
-            } else {
-                res.end('error');
-            }
-        });
+        if(typeof req.body.pic != 'undefined' && req.body.pic != '') {
+            var imageUri = weibo.parseUrltoObj(req.body.pic);
+            var stockcode = req.body.stockcode.substr(-6);
+            var nowTime = new Date().getTime();
+            var extName = path.extname(path.basename(req.body.pic));
+            getWeiboPicFolder('', function(imageFolder) {
+                http.get(imageUri, function(resp) {
+                    var buffers = [], size = 0;
+                    resp.on('data', function(buffer) {
+                        buffers.push(buffer);
+                        size += buffer.length;
+                    });
+                    resp.on('end', function() {
+                        myPic = imageFolder+'/'+stockcode+'_'+nowTime+extName;
+                        var buffer = new Buffer(size), pos = 0;
+                        for(var i = 0, len = buffers.length; i < len; i++) {
+                            buffers[i].copy(buffer, pos);
+                            pos += buffers[i].length;
+                        }
+                        fs.writeFile(myPic, buffer);
+                        weibo.addWeibo(req.body.stockcode, req.body.content, myPic, function(blogid) {
+                            if(blogid > 0) {
+                                res.end('success');
+                            } else {
+                                res.end('error');
+                            }
+                        });
+                    });
+                });
+            });
+        } else {
+            weibo.addWeibo(req.body.stockcode, req.body.content, '', function(blogid) {
+                if(blogid > 0) {
+                    res.end('success');
+                } else {
+                    res.end('error');
+                }
+            });
+        }
     }
 });
 app.listen(9559);
+
+var getWeiboPicFolder = function(date, cb) {
+    if(typeof date != 'string' || date == '') {
+        var myDate = new Date();
+        var yyyy = myDate.getFullYear().toString();
+        var mm = (myDate.getMonth()+1).toString();
+        var dd  = myDate.getDate().toString();
+        date = yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]);
+    }
+    var imageFolder = settings.pic.folder;
+    if(imageFolder.substr(0, 1) != '/') {
+        imageFolder = __dirname + '/' + imageFolder;
+    }
+    imageFolder += '/' + date;
+    fs.stat(imageFolder, function(err, stats) {
+        if(err) {
+            fs.mkdir(imageFolder, 0777, function() {
+                cb(imageFolder);
+            });
+        }
+        cb(imageFolder);
+    });
+}
