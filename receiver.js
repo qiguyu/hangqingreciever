2dcc60b40f2d23684232feb5e11f159ddbc739d3
wzy <yoyo_524@hotmail.com>
2012-02-01 10:57:26 +0800

    1、行情接收后改为压队列 2、图片改成node生成

--- receiver.js
+++ receiver.js
@@ -1,6 +1,7 @@
 var settings = require('./etc/settings.json');
 var request = require('request');
 var weibo = require('./lib/weibo');
+var zjlx = require('./lib/zjlx');
 var template = require('./lib/template');
 var http = require('http');
 var fs = require('fs');
@@ -8,104 +9,33 @@ var path = require('path');
 var express = require('express');
 var app = express.createServer();
 
+var accounts; //保存所有有账号的股票代码
+weibo.getAccounts(function(err, acc){
+    if(err){
+        console.log(['load valid stock_code error:', err]);
+        return;
+    }
+    accounts = acc; 
+});
+
+var async = require('async');
+
 app.use(express.bodyParser());
 app.post('/hangqing', function(req, res) {
     var myDate = new Date();
     var myHour = myDate.getHours();
     var myMin = myDate.getMinutes();
-    if(req.body.stockcode == undefined || req.body.name == undefined || req.body.date == undefined || req.body.stockcode.length != 8) {
+    if(!req.body.stockcode || req.body.name == undefined || req.body.date == undefined || req.body.stockcode.length != 8) {
         res.end('error! invalid stock code or name');
     } else if(myHour > 15 || myHour < 9 || (myHour == 15 && myMin > 30) || (myHour == 9 && myMin < 20)) {
         res.end('not work now!');
     } else {
-        var stockcode = req.body.stockcode.substr(-6);
-        var sToday = req.body.date.substr(0, 8);
-        var hqHour = parseInt(req.body.date.substr(8, 2));
-        var hqData = weibo.formatHqNumbers(req.body);
-        if(hqHour < 11) {
-            hqData.showtype = '开盘';
-        } else if(hqHour > 13) {
-            hqData.showtype = '收盘';
-        } else {
-            hqData.showtype = '午盘';
+        if(accounts && !accounts[req.body.stockcode.toLowerCase()]){
+            res.end('error! invalid stock code ');
+            return;
         }
-        if(hqHour < 11) {
-            hqData.code = stockcode;
-            hqData.sDate = req.body.date.substr(4, 2)+'月'+req.body.date.substr(6, 2)+'日';
-            var tplName = 'kaipan.tpl';
-            if(hqData.openUpown == 0) {
-                var tplName = 'pingkai.tpl';
-            } else if(Math.abs(hqData.openMarkup) < 1) {
-                hqData.fudu = '微幅';
-                if(hqData.openMarkup < 0) {
-                    hqData.fudu += '下跌';
-                } else {
-                    hqData.fudu += '上涨';
-                }
-            } else {
-                hqData.fudu = '跳空';
-                if(hqData.openMarkup < 0) {
-                    hqData.fudu += '低开';
-                } else {
-                    hqData.fudu += '高开';
-                }
-                if(Math.abs(hqData.openMarkup) > 3) {
-                    hqData.fudu = '大幅' + hqData.fudu;
-                }
-            }
-            hqData.openUpown = Math.abs(hqData.openUpown).toFixed(2);
-            hqData.openMarkup = Math.abs(hqData.openMarkup).toFixed(2);
-            var content = template.display(tplName, hqData);
-            weibo.addWeibo(req.body.stockcode, content, '', function(blogid) {
-                if(blogid > 0) {
-                    res.end('success');
-                } else {
-                    res.end('error');
-                }
-            });
-        } else {
-            var content = template.display('shoupan.tpl', hqData);
-            if( hqData.type == 1) {
-                request({ uri:settings.zjlx.api+stockcode }, function (error, response, body) {
-                    if(error || response.statusCode != 200) {
-                        console.log('request error:'+stockcode);
-                    }
-                    try {
-                        var myPic = '';
-                        var imageUri = weibo.parseUrltoObj(settings.zjlx.image + stockcode);
-                        getWeiboPicFolder(sToday, function(imageFolder) {
-                            myPic = imageFolder+'/'+stockcode+'_zjlx_'+hqHour+'.png';
-                            fetchWeiboPic(imageUri, myPic, function() {
-                                weibo.addWeibo(req.body.stockcode, content, myPic, function(blogid) {
-                                    if(blogid > 0) {
-                                        res.end('success');
-                                    } else {
-                                        res.end('error');
-                                    }
-                                });
-                            });
-                        });
-                    } catch(err) {
-                        console.log('parse error12:'+stockcode);
-                        weibo.addWeibo(req.body.stockcode, content, '', function(blogid) {
-                            if(blogid > 0) {
-                                res.end('success');
-                            } else {
-                                res.end('error');
-                            }
-                        });
-                    }
-                });
-            } else {
-                weibo.addWeibo(req.body.stockcode, content, '', function(blogid) {
-                    if(blogid > 0) {
-                        res.end('success');
-                    } else {
-                        res.end('error');
-                    }
-                });
-            }
-        }
+        hqQueue.push(req.body);
+        res.end('success');
     }
 });
 app.post('/weibo', function(req, res) {
@@ -183,3 +113,74 @@ var fetchWeiboPic = function(imageUri, picPath, cb) {
         });
     });
 }
+var composite = function(body, callback){
+    var echoResult = function(blogid){
+        if(blogid > 0) {
+            console.log('receive hangqing success,blog id is ' + blogid);
+        } else {
+            console.log('receive hangqing failure:' + JSON.stringify(body));
+        }   
+        console.log('current queue length:'+hqQueue.length());
+        callback();
+    }
+    var stockcode = body.stockcode.substr(-6);
+    var sToday = body.date.substr(0, 8);
+    var hqHour = parseInt(body.date.substr(8, 2));
+    var hqData = weibo.formatHqNumbers(body);
+    if(hqHour < 11) {
+        hqData.showtype = '开盘';
+    } else if(hqHour > 13) {
+        hqData.showtype = '收盘';
+    } else {
+        hqData.showtype = '午盘';
+    }
+    if(hqHour < 11) {
+        hqData.code = stockcode;
+        hqData.sDate = body.date.substr(4, 2)+'月'+body.date.substr(6, 2)+'日';
+        var tplName = 'kaipan.tpl';
+        if(hqData.openUpown == 0) {
+            var tplName = 'pingkai.tpl';
+        } else if(Math.abs(hqData.openMarkup) < 1) {
+            hqData.fudu = '微幅';
+            if(hqData.openMarkup < 0) {
+                hqData.fudu += '下跌';
+            } else {
+                hqData.fudu += '上涨';
+            }
+        } else {
+            hqData.fudu = '跳空';
+            if(hqData.openMarkup < 0) {
+                hqData.fudu += '低开';
+            } else {
+                hqData.fudu += '高开';
+            }
+            if(Math.abs(hqData.openMarkup) > 3) {
+                hqData.fudu = '大幅' + hqData.fudu;
+            }
+        }
+        hqData.openUpown = Math.abs(hqData.openUpown).toFixed(2);
+        hqData.openMarkup = Math.abs(hqData.openMarkup).toFixed(2);
+        var content = template.display(tplName, hqData);
+        weibo.addWeibo(body.stockcode, content, '', function(blogid) {
+            echoResult(blogid);
+        });
+    } else {
+        var content = template.display('shoupan.tpl', hqData);
+        if( hqData.type == 1) {
+            getWeiboPicFolder(sToday, function(imageFolder) {
+                myPic = imageFolder+'/'+stockcode+'_zjlx_'+hqHour+'.png';
+                zjlx.drawZjlxImg(stockcode, myPic, function() {
+                    weibo.addWeibo(body.stockcode, content, myPic, function(blogid) {
+                        echoResult(blogid);
+                    });
+                });
+            });
+        } else {
+            weibo.addWeibo(body.stockcode, content, '', function(blogid) {
+                echoResult(blogid);
+            });
+        }
+    }   
+}
+
+var hqQueue = async.queue(composite, 5);
