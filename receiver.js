a6cbf47aa55f7c0d4e7e2980637eb9210eb11e94
wzy <yoyo_524@hotmail.com>
2012-02-02 20:27:09 +0800

    代码优化

--- receiver.js
+++ receiver.js
@@ -4,7 +4,6 @@ var weibo = require('./lib/weibo');
 var zjlx = require('./lib/zjlx');
 var template = require('./lib/template');
 var http = require('http');
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var app = express.createServer();
@@ -12,7 +11,7 @@ var app = express.createServer();
 var accounts; //保存所有有账号的股票代码
 weibo.getAccounts(function(err, acc){
     if(err){
-        console.log(['load valid stock_code error:', err]);
+        console.log(['[error] load valid stock_code error:', err]);
         return;
     }
     accounts = acc; 
@@ -26,15 +25,14 @@ app.post('/hangqing', function(req, res) {
     var myHour = myDate.getHours();
     var myMin = myDate.getMinutes();
     if(!req.body.stockcode || req.body.name == undefined || req.body.date == undefined || req.body.stockcode.length != 8) {
+        console.log('[error] invalid stock code or name:', JSON.stringify(req.body));
         res.end('error! invalid stock code or name');
     } else if(myHour > 15 || myHour < 9 || (myHour == 15 && myMin > 30) || (myHour == 9 && myMin < 20)) {
         res.end('not work now!');
     } else {
-        if(accounts && !accounts[req.body.stockcode.toLowerCase()]){
-            res.end('error! invalid stock code ');
-            return;
+        if(accounts && accounts[req.body.stockcode.toLowerCase()]){
+            hqQueue.push(req.body);
         }
-        hqQueue.push(req.body);
         res.end('success');
     }
 });
@@ -47,9 +45,9 @@ app.post('/weibo', function(req, res) {
             var stockcode = req.body.stockcode.substr(-6);
             var nowTime = new Date().getTime();
             var extName = path.extname(path.basename(req.body.pic));
-            getWeiboPicFolder('', function(imageFolder) {
+            weibo.getWeiboPicFolder(function(imageFolder) {
                 var myPic = imageFolder+'/'+stockcode+'_'+nowTime+extName;
-                fetchWeiboPic(imageUri, myPic, function() {
+                weibo.fetchWeiboPic(imageUri, myPic, function() {
                     weibo.addWeibo(req.body.stockcode, req.body.content, myPic, function(blogid) {
                         if(blogid > 0) {
                             res.end('success');
@@ -72,55 +70,16 @@ app.post('/weibo', function(req, res) {
 });
 app.listen(9559);
 
-var getWeiboPicFolder = function(date, cb) {
-    if(typeof date != 'string' || date == '') {
-        var myDate = new Date();
-        var yyyy = myDate.getFullYear().toString();
-        var mm = (myDate.getMonth()+1).toString();
-        var dd  = myDate.getDate().toString();
-        date = yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]);
-    }
-    var imageFolder = settings.pic.folder;
-    if(imageFolder.substr(0, 1) != '/') {
-        imageFolder = __dirname + '/' + imageFolder;
-    }
-    imageFolder += '/' + date;
-    fs.stat(imageFolder, function(err, stats) {
-        if(err) {
-            fs.mkdir(imageFolder, 0777, function() {
-                cb(imageFolder);
-            });
-        }
-        cb(imageFolder);
-    });
-}
-
-var fetchWeiboPic = function(imageUri, picPath, cb) {
-    http.get(imageUri, function(resp) {
-        var buffers = [], size = 0;
-        resp.on('data', function(buffer) {
-            buffers.push(buffer);
-            size += buffer.length;
-        });
-        resp.on('end', function() {
-            var buffer = new Buffer(size), pos = 0;
-            for(var i = 0, len = buffers.length; i < len; i++) {
-                buffers[i].copy(buffer, pos);
-                pos += buffers[i].length;
-            }
-            fs.writeFile(picPath, buffer);
-            cb();
-        });
-    });
-}
 var composite = function(body, callback){
-    var echoResult = function(blogid){
+    var si = setTimeout(callback, 30000);
+    var echoResult = function(blogid, content, image){
+        clearTimeout(si);
         if(blogid > 0) {
-            console.log('receive hangqing success,blog id is ' + blogid);
+            console.log('[message] receive hangqing success,blog id is ' + blogid + ',content:'+content+'image:'+image);
         } else {
-            console.log('receive hangqing failure:' + JSON.stringify(body));
+            console.log('[error] receive hangqing failure:' + JSON.stringify(body));
         }   
-        console.log('current queue length:'+hqQueue.length());
+        console.log('[debug] current queue length:'+hqQueue.length());
         callback();
     }
     var stockcode = body.stockcode.substr(-6);
@@ -162,30 +121,27 @@ var composite = function(body, callback){
         hqData.openMarkup = Math.abs(hqData.openMarkup).toFixed(2);
         var content = template.display(tplName, hqData);
         weibo.addWeibo(body.stockcode, content, '', function(blogid) {
-            echoResult(blogid);
+            echoResult(blogid, content, '');
         });
     } else {
         var content = template.display('shoupan.tpl', hqData);
         if( hqData.type == 1) {
-            getWeiboPicFolder(sToday, function(imageFolder) {
-                var myPic = imageFolder+'/'+stockcode+'_zjlx_'+hqHour+'.png';
-                zjlx.drawZjlxImg(stockcode, myPic, function(err, oData) {
-                    if(!err) {
-                        var zjData = weibo.formatZjlxNumbers(oData);
-                        content += template.display('zjlx.tpl', zjData);
-                        weibo.addWeibo(body.stockcode, content, myPic, function(blogid) {
-                            echoResult(blogid);
-                        });
-                    } else {
-                        weibo.addWeibo(body.stockcode, content, '', function(blogid) {
-                            echoResult(blogid);
-                        });
-                    }
-                });
+            zjlx.drawZjlxImg(stockcode, function(err, oData, myPic) {
+                if(!err) {
+                    var zjData = weibo.formatZjlxNumbers(oData);
+                    content += template.display('zjlx.tpl', zjData);
+                    weibo.addWeibo(body.stockcode, content, myPic, function(blogid) {
+                        echoResult(blogid, content, myPic);
+                    });
+                } else {
+                    weibo.addWeibo(body.stockcode, content, '', function(blogid) {
+                        echoResult(blogid, content, '');
+                    });
+                }
             });
         } else {
             weibo.addWeibo(body.stockcode, content, '', function(blogid) {
-                echoResult(blogid);
+                echoResult(blogid, content, '');
             });
         }
     }   
